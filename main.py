35a723578674a2a546c29dc02668405a022ac952
Dominik Freunberger <dominik.freunberger@gmail.com>
2023-02-13 17:22:27 +0100

    Add argparser

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+import argparse
+
 from scripts.tokenizer import WordTokenizer
 from scripts.wordcloud import WordCloudGenerator
+from scripts.scrape import scrape_pubmed
 
+if __name__ == '__main__':
 
-tokenizer = WordTokenizer(lemmatize=False)
+    parser = argparse.ArgumentParser('main.py')
+    parser.add_argument('-e', '--email', type=str, help='Email address for PubMed account')
+    parser.add_argument('-q', '--query', type=str, default="bilingual*", help='Keyword for PubMed query; wildcards (*) recommended')
+    parser.add_argument('-s', '--stopwords', type=str, nargs="+", help='Additional stopwords')
 
-def main():
-    
-    additional_stopwords = [
-                        "bilingual",
-                       "bilingualism",
-                       "bilinguals",
-                       "bilingually",
-                       "monolingual",
-                       "monolinguals",
-                       "monolingualbilingual",
-                       "language",
-                       "children"
-                       #"study"
-                       ]
+    args = parser.parse_args()
 
+    tokenizer = WordTokenizer(lemmatize=False)
+    scrape_pubmed(query = args.query, email = args.email)
     data = pd.read_csv("./data/all_titles.csv")
 
     data['decade'] = np.floor_divide(pd.DatetimeIndex(data['publication_date']).year, 10).astype(int) * 10
-
     data["tokens"] = tokenizer.transform(data["title"])
 
-    make_wordcloud = WordCloudGenerator(stopwords=additional_stopwords)
+    make_wordcloud = WordCloudGenerator(stopwords=args.stopwords)
 
     for decade in list(data["decade"].unique()):
         make_wordcloud.generate_wordcloud(data["tokens"][data["decade"]==decade].tolist(), f"./output/{decade}s.png")
 
-if __name__ == '__main__':
-    main()
+    print("Finished! Check your wordclouds in the output folder.")
+
+
+
+
--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,21 +1,26 @@
 import pandas as pd
 from pymed import PubMed
 
-pubmed = PubMed(tool="MyTool", email=email)
+def scrape_pubmed(query: str,
+                  email: str):
+    
+    pubmed = PubMed(tool="WordCloudTrends", email=email)
+    
+    query_string = f'("{query}"[All Fields]) AND (1950:2023[pdat])'
 
-query = '("bilingual*"[All Fields]) AND (1950:2023[pdat])'
+    print("Scraping PubMed...")
+    results = pubmed.query(query_string, max_results=9998)
+    articles = []
+    article_info = []
 
-results = pubmed.query(query, max_results=10000)
-articles = []
-article_info = []
-
-for article in results:
-    article_dict = article.toDict()
-    articles.append(article_dict)
-
-for article in articles:
-    pubmedId = article['pubmed_id'].partition('\n')[0]
-    article_info.append({u'title':article['title'],
-                       u'publication_date':article['publication_date']})
-
-pd.DataFrame.from_dict(article_info).to_csv ('./data/all_titles.csv') 
+    for article in results:
+        article_dict = article.toDict()
+        articles.append(article_dict)
+    print("Tidying data...")
+    for article in articles:
+        pubmedId = article['pubmed_id'].partition('\n')[0]
+        article_info.append({u'title':article['title'],
+                        u'publication_date':article['publication_date']})
+    
+    pd.DataFrame.from_dict(article_info).to_csv ('./data/all_titles.csv') 
+    print("Done.")
